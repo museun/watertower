9e3508d91738b00757e448dd2d4350b8ecc39326
museun <museun@outlook.com>
2019-05-14 03:25:37 -0400

    move stuff around

--- src/parse/constant.rs
+++ src/parse/constant.rs
@@ -28,6 +28,86 @@ impl ConstantIndex {
 }
 
 #[derive(Debug, Clone)]
+pub enum Constant {
+    Integer(u32),
+    Float(f32),
+    Long(u64),
+    Double(f64),
+    Utf8(String),
+
+    ClassRef(ConstantIndex),
+    StringRef(ConstantIndex),
+
+    MethodRef(MethodRef),
+    FieldRef(FieldRef),
+    InterfaceMethodRef(InterfaceMethodRef),
+    NameAndTypeRef(NameAndTypeRef),
+    InvokeDynamicRef(InvokeDynamicRef),
+
+    MethodHandleRef(MethodHandle),
+    MethodType(ConstantIndex),
+
+    // for padding
+    Padding,
+}
+
+impl<R: Read> ReadType<'_, R> for Constant {
+    type Output = Self;
+    type Context = NullContext;
+    fn read(reader: &mut Reader<'_, R>, context: &Self::Context) -> Result<Self> {
+        macro_rules! read_map {
+            ($left:ident => $right:ident) => {
+                $left::read(reader, context).map(Constant::$right)
+            };
+            ($left:ident, $name:expr => $right:ident) => {
+                reader.$left(stringify!($name)).map(Constant::$right)
+            };
+        }
+
+        match reader.read_u8("tag")? {
+            1 => read_utf8(reader).map(Constant::Utf8),
+            3 => read_map!(read_u32, "integer" => Integer),
+            4 => read_map!(read_f32, "float" => Float),
+            5 => read_map!(read_u64, "long" => Long),
+            6 => read_map!(read_f64, "double" => Double),
+            7 => read_map!(ConstantIndex => ClassRef),
+            8 => read_map!(ConstantIndex => StringRef),
+            9 => read_map!(FieldRef => FieldRef),
+            10 => read_map!(MethodRef => MethodRef),
+            11 => read_map!(InterfaceMethodRef => InterfaceMethodRef),
+            12 => read_map!(NameAndTypeRef => NameAndTypeRef),
+            15 => read_map!(MethodHandle => MethodHandleRef),
+            16 => read_map!(ConstantIndex => MethodType),
+            18 => read_map!(InvokeDynamicRef => InvokeDynamicRef),
+            e => Err(Error::UnknownTag(e)),
+        }
+    }
+}
+
+impl Constant {
+    #[inline]
+    pub fn get_tag(self) -> Option<u8> {
+        match self {
+            Constant::Utf8(..) => Some(1),
+            Constant::Integer(..) => Some(3),
+            Constant::Float(..) => Some(4),
+            Constant::Long(..) => Some(5),
+            Constant::Double(..) => Some(6),
+            Constant::ClassRef(..) => Some(7),
+            Constant::StringRef(..) => Some(8),
+            Constant::FieldRef(..) => Some(9),
+            Constant::MethodRef(..) => Some(10),
+            Constant::InterfaceMethodRef(..) => Some(11),
+            Constant::NameAndTypeRef(..) => Some(12),
+            Constant::MethodHandleRef(..) => Some(15),
+            Constant::MethodType(..) => Some(16),
+            Constant::InvokeDynamicRef(..) => Some(18),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct MethodRef {
     class: ConstantIndex,
     name_and_type: ConstantIndex,
@@ -109,86 +189,6 @@ impl<R: Read> ReadType<'_, R> for InvokeDynamicRef {
             bootstrap: MethodIndex::read(reader, context)?,
             name_and_type: ConstantIndex::read(reader, context)?,
         })
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum Constant {
-    Integer(u32),
-    Float(f32),
-    Long(u64),
-    Double(f64),
-    Utf8(String),
-
-    ClassRef(ConstantIndex),
-    StringRef(ConstantIndex),
-
-    MethodRef(MethodRef),
-    FieldRef(FieldRef),
-    InterfaceMethodRef(InterfaceMethodRef),
-    NameAndTypeRef(NameAndTypeRef),
-    InvokeDynamicRef(InvokeDynamicRef),
-
-    MethodHandleRef(MethodHandle),
-    MethodType(ConstantIndex),
-
-    // for padding
-    Padding,
-}
-
-impl<R: Read> ReadType<'_, R> for Constant {
-    type Output = Self;
-    type Context = NullContext;
-    fn read(reader: &mut Reader<'_, R>, context: &Self::Context) -> Result<Self> {
-        macro_rules! read_map {
-            ($left:ident => $right:ident) => {
-                $left::read(reader, context).map(Constant::$right)
-            };
-            ($left:ident, $name:expr => $right:ident) => {
-                reader.$left(stringify!($name)).map(Constant::$right)
-            };
-        }
-
-        match reader.read_u8("tag")? {
-            1 => read_utf8(reader).map(Constant::Utf8),
-            3 => read_map!(read_u32, "integer" => Integer),
-            4 => read_map!(read_f32, "float" => Float),
-            5 => read_map!(read_u64, "long" => Long),
-            6 => read_map!(read_f64, "double" => Double),
-            7 => read_map!(ConstantIndex => ClassRef),
-            8 => read_map!(ConstantIndex => StringRef),
-            9 => read_map!(FieldRef => FieldRef),
-            10 => read_map!(MethodRef => MethodRef),
-            11 => read_map!(InterfaceMethodRef => InterfaceMethodRef),
-            12 => read_map!(NameAndTypeRef => NameAndTypeRef),
-            15 => read_map!(MethodHandle => MethodHandleRef),
-            16 => read_map!(ConstantIndex => MethodType),
-            18 => read_map!(InvokeDynamicRef => InvokeDynamicRef),
-            e => Err(Error::UnknownTag(e)),
-        }
-    }
-}
-
-impl Constant {
-    #[inline]
-    pub fn get_tag(self) -> Option<u8> {
-        match self {
-            Constant::Utf8(..) => Some(1),
-            Constant::Integer(..) => Some(3),
-            Constant::Float(..) => Some(4),
-            Constant::Long(..) => Some(5),
-            Constant::Double(..) => Some(6),
-            Constant::ClassRef(..) => Some(7),
-            Constant::StringRef(..) => Some(8),
-            Constant::FieldRef(..) => Some(9),
-            Constant::MethodRef(..) => Some(10),
-            Constant::InterfaceMethodRef(..) => Some(11),
-            Constant::NameAndTypeRef(..) => Some(12),
-            Constant::MethodHandleRef(..) => Some(15),
-            Constant::MethodType(..) => Some(16),
-            Constant::InvokeDynamicRef(..) => Some(18),
-            _ => None,
-        }
     }
 }
 
